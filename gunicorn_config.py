2e45fb2a86890f23b7127a11c8e4c077a58b7dc9
yuikairi <yhmtc@icloud.com>
2023-12-25 19:53:33 +0900

    やり直し

---
+++ gunicorn_config.py
@@ -0,0 +1,17 @@
+# gunicorn_config.py
+
+import multiprocessing
+
+bind = '0.0.0.0:8080'  # アプリケーションがリッスンするIPアドレスとポート
+workers = multiprocessing.cpu_count() * 2 + 1  # ワーカープロセスの数（CPUのコア数に応じて調整）
+
+# Djangoアプリケーションのディレクトリを指定
+chdir = '/Users/yamazakiyuii/Documents/playful-sennari/myproject'# WSGIアプリケーションのエントリーポイントを指定
+module = 'app.wsgi:application'
+
+# エラーログとアクセスログのパスを指定
+errorlog = '/var/log/gunicorn/error.log'
+accesslog = '/var/log/gunicorn/access.log'
+
+# デーモンモードで実行する場合はコメントを解除
+# daemon = True
