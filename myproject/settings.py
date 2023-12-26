2e7196fc952168431ae39ceac629658808dffbad
yuikairi <yhmtc@icloud.com>
2023-12-26 19:03:52 +0900

    settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,7 +14,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
